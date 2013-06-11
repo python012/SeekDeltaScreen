df538edbf0099f04a6340c0b217d0b5bcbdc1554
Reed Xia <huaqin2005@gmail.com>
2013-06-11 11:09:19 +0900

    Update test.py
    Feature complete.

--- test.py
+++ test.py
@@ -19,34 +19,34 @@ OLD_SCREEN_FILE_DIR = None
 NEW_SCREEN_FILE_DIR = None
 
 # Control values
-REQUIRE_GENERATE_OLD_SCREENLIB = False
-REQUIRE_GENERATE_NEW_SCREENLIB = False
+REQUIRE_GENERATE_OLD_SCREENLIB = True
+REQUIRE_GENERATE_NEW_SCREENLIB = True
 
 scripter_dict = {
-                        "wcm348" : "Tina Xu",
-                        "mdrk73" : "Beth Du",
-                        "ghn368" : "Timmy Li",
-                        "kmw348" : "Nancy Huang",
-                        "rngp76" : "Allen Xu",
-                        "gtrm74" : "Ricky Xiong",
-                        "tdc786" : "Gao Ran",
-                             "luyi" : "Lu Yi",
-                        "thp738" : "Amilia Li",
-                        "qgxc76" : "Tracy Shen",
-                        "gwj384" : "Isabella Niu",
-                        "tmq368" : "Fang Hepeng",
-                        "nxw846" : "Vera Wei",
-                        "dbpk83" : "Li Haoyu",
-                         "xft437" : "John Qin",
-                        "kpj836" : "Jenifer Tong",
-                        "pjg867" : "Wang Xiao Chun",
-                        "rbc436" : "Hunter Yin",
-                        "nhb468" : "Liu Xiujuan",
-                        "vkcr36" : "Billy Li",
-                        "tjwm46" : "Monica Yin",
-                              "lily": "Lily Liu",
-                        "vpgb86": "Barkley Li"
-                        }
+                      "wcm348" : "Tina Xu",
+                      "mdrk73" : "Beth Du",
+                      "ghn368" : "Timmy Li",
+                      "kmw348" : "Nancy Huang",
+                      "rngp76" : "Allen Xu",
+                      "gtrm74" : "Ricky Xiong",
+                      "tdc786" : "Gao Ran",
+                      "luyi" : "Lu Yi",
+                      "thp738" : "Amilia Li",
+                      "qgxc76" : "Tracy Shen",
+                      "gwj384" : "Isabella Niu",
+                      "tmq368" : "Fang Hepeng",
+                      "nxw846" : "Vera Wei",
+                      "dbpk83" : "Li Haoyu",
+                      "xft437" : "John Qin",
+                      "kpj836" : "Jenifer Tong",
+                      "pjg867" : "Wang Xiao Chun",
+                      "rbc436" : "Hunter Yin",
+                      "nhb468" : "Liu Xiujuan",
+                      "vkcr36" : "Billy Li",
+                      "tjwm46" : "Monica Yin",
+                      "lily": "Lily Liu",
+                      "vpgb86": "Barkley Li"
+                      }
 
 def initializeOldScreenlib():
   print "****** Start to initialize Old Screenlib ******"
@@ -74,7 +74,7 @@ def initializeOldScreenlib():
       conn.commit()
       conn.close()
     conn.close()
-    print "Analysing old screens " + str(i) + " / " + str(len(OLD_SCREEN_FILE_DIR))
+    print "Analysed old sqlites " + str(i) + " / " + str(len(OLD_SCREEN_FILE_DIR))
 
 def initializeNewScreenlib():
   print "****** Start to initialize New Screenlib ******"
@@ -104,7 +104,7 @@ def initializeNewScreenlib():
       conn.close()
     conn.close()
 
-    print "Analysing new screens " + str(i) + " / " + str(len(NEW_SCREEN_FILE_DIR))
+    print "Analysed new sqlites " + str(i) + " / " + str(len(NEW_SCREEN_FILE_DIR))
 
 def seekDeltaScreen():
   new_conn = sqlite3.connect(NEW_SCREENLIB_PATH)
@@ -121,7 +121,7 @@ def seekDeltaScreen():
   new_screen_set =  new_screen_set - old_screen_set # screens in new screeen set but not in old screen sets
   
   delta_screen_page = PyH('Check screen list change bewteen AViK Rounds')
-  delta_screen_page << h1('Missing Screen List', cl='center')
+  delta_screen_page << h1('Missing Screen List')
   
   if (0 == len(missing_screen_set)):
     delta_screen_page << p('No missing screen found.')
@@ -136,10 +136,38 @@ def seekDeltaScreen():
       old_cur.execute("select scriptname, build, tags from screenlib where screenname = " + '\'' + i + '\'')
       for j in old_cur.fetchall():
         tr1 << td(str(index)) << td(str(i)) << td(j[0]) << td(j[1])
-        tr1 << td(scripter_dict[eval(j[2])['executor'].lower()])
+        tag_dict = eval(j[2])
+        if 'executor' in tag_dict:
+          tr1 << td(scripter_dict[eval(j[2])['executor'].lower()])
+        else:
+          tr1 << td('null')
       index +=1
-    delta_screen_page.printOut("test_delt_page.html")
-    webbrowser.open("test_delt_page.html")
+  old_conn.close()
+
+  delta_screen_page << h1('New Screen List')
+  if (0 == len(new_screen_set)):
+    delta_screen_page << p('No new screen found.')
+  else:
+    new_screen_tab = delta_screen_page << table(border = "1")
+    tr2 = new_screen_tab << tr()
+    tr2 << td('No.') + td('Screen') + td('Script') + td('Label') + td('Scriptor')
+
+    index = 1 # Give index to table of missing screen
+    for i in new_screen_set:
+      tr3 = new_screen_tab << tr()
+      new_cur.execute("select scriptname, build, tags from screenlib where screenname = " + '\'' + i + '\'')
+      for f in new_cur.fetchall():
+        tr3 << td(str(index)) << td(str(i)) << td(f[0]) << td(f[1])
+        tag_dict = eval(f[2])
+        if 'executor' in tag_dict:
+          tr3 << td(scripter_dict[tag_dict['executor'].lower()])
+        else:
+          tr3 << td('null')
+      index +=1
+  new_conn.close()
+  
+  delta_screen_page.printOut("test_delt_page.html")
+  webbrowser.open("test_delt_page.html")
     
   
 def main():
